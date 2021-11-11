f059afe32581a2c0a3937ad188edc61fa38447ca
Fábio <fabiomirandafig@gmail.com>
2021-11-10 22:19:12 -0300

    Pokedex

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+      <img src="./assets/logo.png">
+      <hr>
+      <h4 class="is-size-4">Pokedex</h4>
+      <input type="text" placeholder="Buscar pokemon pelo nome" v-model="busca" class="input is-rounded">
+      <button class="button is-fullwidth is-success" id="BuscaBtn" @click="buscar">Buscar</button>
+      <div v-for="(poke,index) in filteredPokemons" :key="poke.url">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+      </div>
+    </div>
+  </div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import Pokemon from './components/Pokemon.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Pokemon
+  },
+  data() {
+    return {
+      pokemons: [],
+      filteredPokemons: [],
+      busca: '',
+    }
+  },
+  created: function() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
+      this.pokemons = res.data.results;
+      this.filteredPokemons = res.data.results;
+      // console.log(this.pokemons);
+    })
+  },
+  computed: {
+    resultadoBusca () {
+      if(this.busca == '' || this.busca == ' ') {
+        return this.pokemons;
+      }
+      else {
+        return this.pokemons.filter(pokemon => pokemon.name == this.busca);
+      }
+    }
+  },
+  methods: {
+    buscar() {
+     this.filteredPokemons = this.pokemons;
+      if(this.busca == '' || this.busca == ' ') {
+       this.filteredPokemons = this.pokemons;
+     } 
+     else {
+       this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca);
+     }
+    }
   }
 }
 </script>
@@ -23,4 +68,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#BuscaBtn {
+  margin-top: 2%;
+}
 </style>
+
+//create retorna uma promise
